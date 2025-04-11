10bb01844b2c39ed82d878b11b50ef72eefd74fc
nazrinbakirova <nazrinbakirova07@gmail.com>
2025-04-11 23:41:01 +0400

    LAB15

--- LABSINREACT/lab15/src/components/buttons/autoButton/autoBtn.jsx
+++ LABSINREACT/lab15/src/components/buttons/autoButton/autoBtn.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 
 const AutoBtn = ({ setAutoCounting }) => {
- return  <button onClick={()=>setAutoCounting((prev)=>!prev)}>start-end</button>
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    if (active) {
+      setActive(false);
+      setAutoCounting(false);
+    } else {
+      setActive(true);
+      setAutoCounting(true);
+    }
+  };
+
+  let buttonText = 'start auto counting';
+  let buttonClass = 'auto-btn';
+
+  if (active) {
+    buttonText = 'stop auto counting';
+    buttonClass += ' active';
+  }
+
+  return (
+    <button className={buttonClass} onClick={handleClick}>
+      {buttonText}
+    </button>
+  );
 };
 
 export default AutoBtn;
--- LABSINREACT/lab15/src/pages/homePage/home.jsx
+++ LABSINREACT/lab15/src/pages/homePage/home.jsx
@@ -3,6 +3,9 @@ import ResetBtn from '../../components/buttons/resetButton/resetBtn'
 import PlusBtn from '../../components/buttons/plusButton/plusBtn'
 import MinusBtn from '../../components/buttons/minusButton/minusBtn'
 import AutoBtn from '../../components/buttons/autoButton/autoBtn'
+import './home.css'
+
+
 
 const Home = () => {
 
@@ -23,16 +26,28 @@ return ()=>
 
   },[autoCounting])
 
+
+  const getClassName = () => {
+    if (count > 0) {
+      return 'counter positive';
+    } else if (count < 0) {
+      return 'counter negative';
+    } else {
+      return 'counter zero';
+    }
+  };
+
+
   return (
-    <div>
-      <h2>{count}</h2>
-<>
+    <div className='main'>
+     <div className={getClassName()}>{count}</div>
+<div className='buttons'>
 <MinusBtn setCount={setCount}/>
 <ResetBtn setCount={setCount}/>
 <PlusBtn  setCount={setCount}/>
 <AutoBtn setAutoCounting={setAutoCounting} />
 
-</>
+</div>
 
     </div>
   )
